925fbfd31438941cdd2d1bc9c8320a76bd4acc85
ariel-codes <as-ariel@outlook.com>
2020-11-20 20:20:05 +0000

    Deploying to gh-pages from  @ 9bafc74d104d23e37ba85c28c3cefa94e0ffb7ba 🚀

--- _nuxt/static/1605833943/cadastro/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605833943",layout:"default",error:b,state:{authenticated:false,error:b,user:{id:a,name:a,email:a}},serverRendered:true,routePath:"\u002Fcadastro",config:{}}}("",null));
