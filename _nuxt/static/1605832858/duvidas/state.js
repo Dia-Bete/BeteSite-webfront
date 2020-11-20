6ef95ec7ec10316db46582a565ae5d6e84bfd7ea
ariel-codes <as-ariel@outlook.com>
2020-11-20 00:41:05 +0000

    Deploying to gh-pages from  @ 81f71f1c2d0845483ab6811952f9a2cfbd08795f 🚀

---
+++ _nuxt/static/1605832858/duvidas/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605832858",layout:"default",error:b,state:{authenticated:false,error:b,user:{id:a,name:a,email:a}},serverRendered:true,routePath:"\u002Fduvidas",config:{}}}("",null));
