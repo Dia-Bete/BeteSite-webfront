408ec008929e8639d2069acfd3b61d0f913bca84
ariel-codes <as-ariel@outlook.com>
2020-11-20 21:18:02 +0000

    Deploying to gh-pages from  @ 26958fe9327164531198ea20a16578656df3824c 🚀

---
+++ _nuxt/static/1605907075/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1605907075",layout:"default",error:b,state:{authenticated:false,error:b,user:{id:a,name:a,email:a}},serverRendered:true,routePath:"\u002Flogin",config:{}}}("",null));
