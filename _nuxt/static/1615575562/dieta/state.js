dadbb142e0baf5a5a35591e2fa3f0d67321dc164
ariel-codes <as-ariel@outlook.com>
2021-03-12 18:59:31 +0000

    Deploying to gh-pages from  @ bef9f07920db24ab6aad59b60dbea93dadd77137 🚀

---
+++ _nuxt/static/1615575562/dieta/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FBeteSite-webfront\u002F_nuxt\u002Fstatic\u002F1615575562",layout:"default",error:a,state:{account:a,bioData:a},serverRendered:true,routePath:"\u002Fdieta",config:{app:{basePath:"\u002FBeteSite-webfront\u002F",assetsPath:"\u002FBeteSite-webfront\u002F_nuxt\u002F",cdnURL:a}}}}(null));
