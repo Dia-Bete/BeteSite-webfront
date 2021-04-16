cb2193604783410520035d11a607c694e79113d4
ThiagoCF05 <thiago.castro.ferreira@gmail.com>
2021-04-16 21:12:45 +0000

    Deploying to gh-pages from  @ dad1c24510f26f756471f63b50567a7e37e9d6bd 🚀

--- _nuxt/static/1615575562/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FBeteSite-webfront\u002F_nuxt\u002Fstatic\u002F1615575562",layout:"default",error:a,state:{account:a,bioData:a},serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002FBeteSite-webfront\u002F",assetsPath:"\u002FBeteSite-webfront\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1618607558/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FBeteSite-webfront\u002F_nuxt\u002Fstatic\u002F1618607558",layout:"default",error:a,state:{account:a,bioData:a},serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002FBeteSite-webfront\u002F",assetsPath:"\u002FBeteSite-webfront\u002F_nuxt\u002F",cdnURL:a}}}}(null));
